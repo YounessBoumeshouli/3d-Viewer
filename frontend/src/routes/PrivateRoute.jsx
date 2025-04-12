08e965cfa0d706d302336b1e6eaf90861afbca12
BoumeshouliYouness <boumeshouliyouness6@gmail.com>
2025-04-12 11:29:47 +0100

    make Private Route for manage the roles in App.jsx

---
+++ frontend/src/routes/PrivateRoute.jsx
@@ -0,0 +1,22 @@
+import { Navigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
+
+const PrivateRoute = ({ children, allowedRoles }) => {
+    const token = localStorage.getItem("token");
+
+    if (!token) return <Navigate to="/home" replace />;
+
+    try {
+        const { role } = jwtDecode(token);
+        if (allowedRoles.includes(role)) {
+            return children;
+        } else {
+            return <Navigate to="/unauthorized" replace />;
+        }
+    } catch (error) {
+        console.error("Invalid token:", error);
+        return <Navigate to="/home" replace />;
+    }
+};
+
+export default PrivateRoute;
