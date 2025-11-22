89e3611cc673c1d086fc6602f8446e5848c1aa18
youness boumeshouli <boumeshouliyouness6@gmail.com>
2025-11-22 12:17:24 +0100

    update

--- backend/app/Http/Controllers/FileUploadController.php
+++ backend/app/Http/Controllers/FileUploadController.php
@@ -19,9 +19,7 @@ class FileUploadController extends Controller
             'price'=>'integer',
             'name'=>'string'
         ]);
-        return response()->json([
-            'message' => 'file uploaded successfully',
-        ], 200);
+
 
         $file = $request->file('file');
         $size = $request->file('file')->getSize() / (1024* 1024) ;
--- frontend/src/components/user/ProjectCard.jsx
+++ frontend/src/components/user/ProjectCard.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import api from '../../services/api.js'
 
-import {useNavigate,useLocation} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 const OfferCard = ({offer}) => {
     const Navigate = useNavigate();
 
     const handlePayment =async (id) =>{
         sessionStorage.setItem('offer_id', id);
-if (id !== 1){
-    const response = await api.post(`/offers/${id}/paypal`);
-        if (response.data && response.data.status === 'success') {
-            window.location.href = response.data.redirect_url;
-        } else {
-            throw new Error('Failed to initialize payment');
+        console.log(id)
+        if (id !== 1){
+            const response = await api.post(`/offers/${id}/paypal`);
+                if (response.data && response.data.status === 'success') {
+                    window.location.href = response.data.redirect_url;
+                } else {
+                    throw new Error('Failed to initialize payment');
+                }
+        }else{
+            Navigate('/payment/success');
         }
-}else{
-    Navigate('/payment/success');
-}
     }
 
 
